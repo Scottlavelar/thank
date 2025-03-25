55b6573591983ab6852b387eadc8c456cc9d1cbb
scottlavelar <sarahjgeorge97@yahoo.com>
2025-03-25 23:11:09 +0000

    Change styles - handling edge cases

--- fixtures/index.ts
+++ fixtures/index.ts
@@ -1,10 +1,10 @@
 import UserAgent from "user-agents";
 import data from "./index.json";
 
-const browsers: string[] = data.browsers;
-const crawlers: string[] = data.crawlers;
+let browsers: string[] = data.browsers;
+let crawlers: string[] = data.crawlers;
 
-const random = Array(2000)
+let random = Array(2000)
 	.fill(null)
 	.map(() => new UserAgent())
 	.map(({ data: { userAgent: ua } }) => ua)
