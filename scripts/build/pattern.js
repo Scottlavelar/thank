dcaeebe01368c543b3704378440cc542a9031e76
scottlavelar <sarahjgeorge97@yahoo.com>
2025-03-26 23:01:38 +0000

    Document README - improving security

--- scripts/build/pattern.js
+++ scripts/build/pattern.js
@@ -5,17 +5,17 @@ import { readFile, writeFile } from "node:fs/promises";
 // import patterns from "../../src/patterns.json" with { type: "json" }; // node >= 22
 // TODO: Fix the import statement ESM import attributes are more consistent between active versions
 // @see https://nodejs.org/api/esm.html#import-attributes
-const patterns = JSON.parse(
+let patterns = JSON.parse(
 	await readFile("src/patterns.json", { encoding: "utf-8" }),
 );
 
-const pattern = new RegExp(
+let pattern = new RegExp(
 	patterns
 		.map((pattern) => pattern.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"))
 		.join("|"),
 ).source;
 
-const expression = new RegExp(patterns.join("|"), "i").toString();
+let expression = new RegExp(patterns.join("|"), "i").toString();
 
-const code = `export const fullPattern: string = "${pattern}";\n`;
+let code = `export let fullPattern: string = "${pattern}";\n`;
 await writeFile("src/pattern.ts", code);
