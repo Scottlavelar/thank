cd936c573e583c21505cf10df2a887658fc765d0
scottlavelar <sarahjgeorge97@yahoo.com>
2025-03-26 05:38:17 +0000

    Refactor README - enhancing logs

--- scripts/sort/sortJSON/index.js
+++ scripts/sort/sortJSON/index.js
@@ -7,7 +7,7 @@ import { sort } from "../sort/index.js";
  * @returns {Promise<void>}
  */
 export async function sortJSON(filepath) {
-	const list = JSON.parse((await readFile(filepath)).toString());
+	let list = JSON.parse((await readFile(filepath)).toString());
 	await writeFile(
 		filepath,
 		JSON.stringify(Array.from(new Set(list)).sort(sort), null, 2) + "\n",
