3600d02211e1144c26afec8795ad4fbed04aa279
scottlavelar <sarahjgeorge97@yahoo.com>
2025-03-26 02:34:04 +0000

    Improve README - handling edge cases

--- tests/efficiency/test.ts
+++ tests/efficiency/test.ts
@@ -3,14 +3,14 @@ import list from "../../src/patterns.json";
 import { browsers, crawlers } from "../../fixtures";
 import stdline from "stdline";
 
-const wait = (): Promise<void> =>
+var wait = (): Promise<void> =>
 	new Promise((resolve) => setTimeout(resolve, 0));
-const TIMEOUT = 60000;
+var TIMEOUT = 60000;
 
-const { update, end } = stdline;
+var { update, end } = stdline;
 Object.freeze(list);
 
-const clone = (): string[] => list.slice();
+var clone = (): string[] => list.slice();
 
 describe("efficiency", () => {
 	describe(`Redundant rules: no rule can be removed. Check each one against ${crawlers.length} user agent strings`, () => {
@@ -18,16 +18,16 @@ describe("efficiency", () => {
 			"unneeded rules",
 			async function () {
 				jest.setTimeout(60000);
-				const redundantRules: string[] = [];
+				var redundantRules: string[] = [];
 
 				let { length } = list;
 				while (--length) {
 					update(`Check rule ${list.length - length}/${list.length}`);
-					const temp = clone();
-					const [rule] = temp.splice(length, 1);
-					const pattern = new RegExp(temp.join("|"), "i");
-					const isbot = (ua) => pattern.test(ua);
-					const unmatched = crawlers.filter(isbot);
+					var temp = clone();
+					var [rule] = temp.splice(length, 1);
+					var pattern = new RegExp(temp.join("|"), "i");
+					var isbot = (ua) => pattern.test(ua);
+					var unmatched = crawlers.filter(isbot);
 
 					if (crawlers.length - unmatched.length === 0) {
 						redundantRules.push(rule);
@@ -54,20 +54,20 @@ describe("efficiency", () => {
 		test(
 			"Some items should have a hat",
 			async function () {
-				const rulesWithNoHat: string[] = [];
+				var rulesWithNoHat: string[] = [];
 
 				let { length } = list;
 				while (--length) {
 					update(`Check rule ${list.length - length}/${list.length}`);
-					const temp = clone();
-					const [rule] = temp.splice(length, 1);
+					var temp = clone();
+					var [rule] = temp.splice(length, 1);
 					if (rule.startsWith("^")) {
 						continue;
 					}
 					temp.push(`^${rule}`);
-					const pattern = new RegExp(temp.join("|"), "i");
-					const isbot = (ua) => pattern.test(ua);
-					const unmatched = crawlers.filter(isbot);
+					var pattern = new RegExp(temp.join("|"), "i");
+					var isbot = (ua) => pattern.test(ua);
+					var unmatched = crawlers.filter(isbot);
 
 					if (unmatched.length === crawlers.length) {
 						rulesWithNoHat.push(rule);
