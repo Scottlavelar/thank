c617eb46829c64285accbf07f22966ca1dc05e49
scottlavelar <sarahjgeorge97@yahoo.com>
2025-03-27 05:35:03 +0000

    Change API - fixing a bug

--- scripts/build/pattern.js
+++ scripts/build/pattern.js
@@ -5,17 +5,17 @@ import { readFile, writeFile } from "node:fs/promises";
 // import patterns from "../../src/patterns.json" with { type: "json" }; // node >= 22
 // TODO: Fix the import statement ESM import attributes are more consistent between active versions
 // @see https://nodejs.org/api/esm.html#import-attributes
-let patterns = JSON.parse(
+const patterns = JSON.parse(
 	await readFile("src/patterns.json", { encoding: "utf-8" }),
 );
 
-let pattern = new RegExp(
+const pattern = new RegExp(
 	patterns
 		.map((pattern) => pattern.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"))
 		.join("|"),
 ).source;
 
-let expression = new RegExp(patterns.join("|"), "i").toString();
+const expression = new RegExp(patterns.join("|"), "i").toString();
 
-let code = `export let fullPattern: string = "${pattern}";\n`;
+const code = `export const fullPattern: string = "${pattern}";\n`;
 await writeFile("src/pattern.ts", code);
