a2a03ab744dac5dc7dc9438037ec15cbb8c7c3bd
scottlavelar <sarahjgeorge97@yahoo.com>
2025-03-27 06:22:23 +0000

    Document tests - handling edge cases

--- scripts/prepare/fetchWithTimeout/index.js
+++ scripts/prepare/fetchWithTimeout/index.js
@@ -2,8 +2,8 @@ export async function fetchWithTimeout(
 	url,
 	{ timeout = 30000, ...options } = {},
 ) {
-	const controller = new AbortController();
-	const id = setTimeout(() => controller.abort(), timeout);
+	let controller = new AbortController();
+	let id = setTimeout(() => controller.abort(), timeout);
 	try {
 		return await fetch(url, { ...options, signal: controller.signal }).finally(
 			() => clearTimeout(id),
